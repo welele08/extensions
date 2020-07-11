a4dac3176c281f7103e6496b0e7d84703aad6593
Ettore Di Giacinto <mudler@gentoo.org>
2020-07-11 15:37:25 +0200

    Fixup sh compatibility

--- extensions/geniso/iso/09_prepare.sh
+++ extensions/geniso/iso/09_prepare.sh
@@ -8,17 +8,19 @@ echo "Removing old kernel artifacts. This may take a while."
 rm -rf $KERNEL_INSTALLED
 mkdir -p $KERNEL_INSTALLED
 BOOT_DIR=$ROOTFS_DIR/boot
-if [[ -L "$BOOT_DIR/bzImage" ]]
-then
+if [[ -L "$BOOT_DIR/bzImage" ]]; then
 
 bz=$(readlink -f $BOOT_DIR/bzImage)
 # Install the kernel file.
 cp $BOOT_DIR/$(basename $bz) \
   $KERNEL_INSTALLED/kernel
+
 else 
 cp $BOOT_DIR/bzImage \
   $KERNEL_INSTALLED/kernel
+
 fi
+
 echo "*** GENERATE ROOTFS BEGIN ***"
 
 
